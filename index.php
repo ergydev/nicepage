9b6abb7358f84338e78172d9330185bee21f963f
ergydev <ergyzenzo.dev@gmail.com>
2022-04-12 11:53:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nice Page</title>
+
+
+    <!-- STYLES  -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- POLICES  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;1,300&family=Roboto&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container">
+        <section class="section_1">
+            <div class="expand_container">
+                <div class="container_layout">
+                    <h1>20<br>22</h1>
+                    <h2>Trends<br>of Design</h2>
+                </div>
+                <div class="img_background">
+                    <img src="assets/img/cloud_background.png" class="img_background_1" alt="une image avec un gradient">
+                </div>
+            </div>
+        </section>
+        <section class="section_2"></section>
+        <section class="section_3"></section>
+        <section class="section_4"></section>
+        <section class="section_5"></section>
+    </div>
+
+</body>
+
+</html>
