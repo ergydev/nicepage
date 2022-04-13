083f803c3bbf38aa4eae11fae943ba32dc23bdd6
ergydev <ergyzenzo.dev@gmail.com>
2022-04-13 16:14:25 +0200

    added all sections

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nice Page</title>
+    <title>Trends Of Design 2022</title>
 
 
     <!-- STYLES  -->
@@ -19,24 +19,140 @@
 </head>
 
 <body>
-    <div class="container">
-        <section class="section_1">
+    <section class="section_1">
+        <div class="container">
             <div class="expand_container">
                 <div class="container_layout">
-                    <h1>20<br>22</h1>
-                    <h2>Trends<br>of Design</h2>
-                </div>
-                <div class="img_background">
-                    <img src="assets/img/cloud_background.png" class="img_background_1" alt="une image avec un gradient">
+                    <h1 class="title_section_1">20<br>22</h1>
+                    <h2 class="text_section_1">Trends<br>of Design</h2>
                 </div>
             </div>
-        </section>
-        <section class="section_2"></section>
-        <section class="section_3"></section>
-        <section class="section_4"></section>
-        <section class="section_5"></section>
-    </div>
+        </div>
+        <div class="img_background">
+            <img src="assets/img/cloud_background.png" class="img_background_1" alt="une image avec un gradient">
+        </div>
+    </section>
 
+    <section class="section_2">
+        <div class="container">
+            <div class="container_section2">
+                <div class="content_section2">
+                    <div class="section_2_text">
+                        <h2 class="title_section">Top Graphic Design Trends 2022 : The Ultimate Guide</h2>
+                        <p class="text_section_2">In the era of digital art, graphic design trends can evaporate as quickly as they emerged. What has been modern for the past few years may look entirely outdated in 2022. While some trends have stood the test of time, others have vanished in the blink of an eye only to make room for new modern looks. So, if you are looking for an antonym of boring, this would be – graphic design trends 2022.</p>
+                    </div>
+                    <div class="section_2_img">
+                        <img src="assets/img/background1.jpg" class="img_section_2" alt="Les design tendances en 2022">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section_3">
+        <div class="container">
+            <div class="content_section3">
+                <div class="section_3_content_1">
+                    <div class="img_section_3_container">
+                        <img src="assets/img/glitch.jpg" class="img_section_3" alt="Photo d'un glich">
+                    </div>
+                    <div class="img_section_3_container">
+                        <img src="assets/img/glitch2.jpg" class="img_section_3" alt="Photo d'un glich">
+                    </div>
+                    <div class="img_section_3_container">
+                        <img src="assets/img/glitch3.jpg" class="img_section_3" alt="Photo d'un glich">
+                    </div>
+                </div>
+                <div class="section_3_text">
+                    <h2 class="title_section">
+                        The "Glitch" Effect
+                    </h2>
+                    <p class="text_section_3">
+                        The corrupted image, i.e. the glitch effect, has been one of the most popular trends in the digital world lately. Apparently, what was once annoying for the spectator has now been turned into a truly wanted effect.
+                    </p>
+                    <p class="text_section_3">
+                        Obviously, horror movie fans have been familiar with this one for ages. Year 2018 is the year when corrupted images take over graphic design world, as well.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section_4">
+        <div class="container">
+            <div class="content_section3">
+                <div class="section_3_content_1">
+                    <div class="img_section_3_container">
+                        <img src="assets/img/art1.jpg" class="img_section_3" alt="Photo d'un glich">
+                    </div>
+                    <div class="img_section_3_container">
+                        <img src="assets/img/art2.jpg" class="img_section_3" alt="Photo d'un glich">
+                    </div>
+                    <div class="img_section_3_container">
+                        <img src="assets/img/art3.jpg" class="img_section_3" alt="Photo d'un glich">
+                    </div>
+                </div>
+                <div class="section_3_text">
+                    <h2 class="title_section">
+                        The “Ruined” Effect
+                    </h2>
+                    <p class="text_section_3">
+                        As far as we can tell, contemporary graphic designers have been obsessed with the “art of destroying”. Everything that includes splashing, scratching, ripping off, breaking or any other form of ruining the aesthetics of a composition is considered modern in 2018.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section_5">
+        <div class="img_background">
+            <img src="assets/img/cloud_background_invert.png" class="img_background_2" alt="Image de nuages avec un dégradé">
+        </div>
+        <div class="container">
+            <div class="content_section5">
+                <div class="content_layout_section5">
+                    <div class="content_left">
+                        <div class="content_left_text">
+                            <h2 class="title_section_5">
+                                Double Light
+                            </h2>
+                        </div>
+                        <div class="content_left_img">
+                            <img src="assets/img/ligtht2.jpg" class="left_img_section_5" alt="Un dégragé de couleurs">
+                        </div>
+                    </div>
+                    <div class="content_right">
+                        <img class="content_right_img" src="assets/img/light.jpg" alt="Une main tenant des lumières">
+                        <p class="content_right_text">An another major “double” among graphic design trends 2018 is the double color light. This effect transforms simple compositions into new edgy, modern looking ones. Double light is an effect that can be achieved with two actual sources of light, or color channel splitting. Here are a few trending examples.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section_6">
+        <div class="container">
+            <div class="content_wrap_6">
+                <div class="content_layout_6">
+                    <div class="content_text_6 border">
+                        <h2 class="title_6">Color Transitions / Gradients</h2>
+                        <p class="text_6">When Instagram changed its logo back in 2016 into a colorful gradient, nobody thought this trend was going to become so huge. It was just the beginning of its rise. Despite the fact that web wasn’t sure about this design technique (everyone was crazy about flat and material back then), here we are, seeing more and more of these colorful gradients.</p>
+                    </div>
+                    <div class="content_img_6 border">
+                        <img src="assets/img/gradient3.jpg" class="img_6" alt="une image avec un dégradé">
+                    </div>
+                    <div class="content_img_6 border">
+                        <img src="assets/img/gradient4.jpg" class="img_6" alt="une image avec un dégradé">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<footer>
+<p>Crée par Ergy Zenzo</p>
+</footer>
 </body>
 
 </html>
